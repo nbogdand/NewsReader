ea9cee8aee0168cb964779dd07f9e9498fb0fa87
Bogdan Noana <nbogdand@yahoo.com>
2019-03-02 14:43:19 +0200

    Fixed webView which loads news' url

--- app/src/main/java/com/example/bogdan/newsapp/DetailArticle.java
+++ app/src/main/java/com/example/bogdan/newsapp/DetailArticle.java
@@ -2,6 +2,9 @@ package com.example.bogdan.newsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class DetailArticle extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class DetailArticle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_article);
+
+        String url = getIntent().getStringExtra("webUrl");
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+
+
     }
 }
